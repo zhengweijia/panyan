9605d58411a14f3deeb1370e7590c54addfd2460
zhengJia <469570531@qq.com>
2017-08-24 22:01:13 +0800

    修改config

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-let host = '64757925.qcloud.la';
+// let host = '64757925.qcloud.la'; //正式
+let host = '58811893.qcloud.la'; //测试
 let config = {
 	staticUrl : 'https://'+host+'/static',
 
